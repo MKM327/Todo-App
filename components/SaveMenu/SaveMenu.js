fa8152387ac2ade9e64d35557ff24dcfd89a3536
MKM327 <85278762+MKM327@users.noreply.github.com>
2022-07-31 16:13:43 +0300

    divided SaveMenu with its own components

--- components/SaveMenu/SaveMenu.js
+++ components/SaveMenu/SaveMenu.js
@@ -1,42 +1,61 @@
 import { useContext } from "react";
 import { TodoContext } from "../../Contexts/TodoContext";
 import TodoInfo from "./TodoInfo";
-const SaveMenu = () => {
+const SaveTodoButton = () => {
   const {
-    menuState,
-    setMenuState,
     addData,
     header,
     description,
     updateData,
     currentItem,
     setCurrentItem,
+    setMenuState,
   } = useContext(TodoContext);
+  const ItemState = !currentItem;
+  return (
+    <div>
+      <button
+        className="input-btn save-menu-btn"
+        onClick={() => {
+          if (ItemState) {
+            addData(header, description);
+          } else {
+            updateData(currentItem.id, header, description);
+            setCurrentItem(undefined);
+            setMenuState();
+          }
+        }}
+      >
+        {ItemState ? "Add" : "Update"}
+      </button>
+    </div>
+  );
+};
+const MenuCloseButton = () => {
+  const { setMenuState, setCurrentItem } = useContext(TodoContext);
+  return (
+    <div className="menu-close">
+      <button
+        onClick={() => {
+          setMenuState();
+          setCurrentItem(undefined);
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+const SaveMenu = () => {
+  const { menuState } = useContext(TodoContext);
   return (
     <div
       className={menuState ? "todo-save-menu open" : "todo-save-menu closed"}
     >
       <div className="save-menu-wrapper">
-        <div className="menu-close">
-          <button onClick={() => setMenuState()}>X</button>
-        </div>
+        <MenuCloseButton />
         <TodoInfo />
-        <div>
-          <button
-            className="input-btn save-menu-btn"
-            onClick={() => {
-              if (!currentItem) {
-                addData(header, description);
-              } else {
-                updateData(currentItem.id, header, description);
-                setCurrentItem(undefined);
-                setMenuState();
-              }
-            }}
-          >
-            Save
-          </button>
-        </div>
+        <SaveTodoButton />
       </div>
     </div>
   );
