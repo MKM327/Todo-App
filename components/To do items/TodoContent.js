eb473f7a408826dfd2e398d310f931d8104c670c
MKM327 <85278762+MKM327@users.noreply.github.com>
2022-08-08 17:59:57 +0300

    added page

--- components/To do items/TodoContent.js
+++ components/To do items/TodoContent.js
@@ -19,17 +19,52 @@ const ButtonRow = () => {
 };
 const TodoContent = () => {
   const { todoData } = useContext(TodoContext);
-
-  const [page, setPage] = useState(1);
+  const numberOfTodoPerPage = 3;
+  const numberOfPages =
+    remainingPages == 0
+      ? Math.floor(todoData.length / numberOfTodoPerPage)
+      : Math.floor(todoData.length / numberOfTodoPerPage) + 1;
+  const remainingPages = todoData.length % 3;
+  console.log(remainingPages);
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <div>
       <ButtonRow />
       <h2>All Notes</h2>
 
       <div className="all-todo">
-        {todoData.map((todoItem) => {
-          return <TodoItem todoItem={todoItem} />;
+        {todoData.map((item, index) => {
+          if (index >= (currentPage - 1) * 3 && index < currentPage * 3) {
+            return <TodoItem key={index} todoItem={item} />;
+          }
         })}
+      </div>
+      <div className="page-row">
+        {remainingPages == 0
+          ? Array.from({ length: numberOfPages }).map((_, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={() => {
+                    setCurrentPage(index + 1);
+                  }}
+                >
+                  {index + 1}
+                </button>
+              );
+            })
+          : Array.from({ length: numberOfPages + 1 }).map((_, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={() => {
+                    setCurrentPage(index + 1);
+                  }}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
       </div>
     </div>
   );
